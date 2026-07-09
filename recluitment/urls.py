609f9dd1cc759e3be70f67e063375aa23825cc9e
MaldoDan18 <137385138+MaldoDan18@users.noreply.github.com>
2026-07-08 21:57:05 -0600

    [MaldoDan18] fix: faltó coma en urls.py xd

--- recluitment/urls.py
+++ recluitment/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.hello_world, name='hello_world'),
     path('login/', view=views.login_view, name='pages_login'),
     path('register/', view=views.register, name='pages_registro'),
-    path('dashboard/', view=views.dashboard, name='dashboard')
+    path('dashboard/', view=views.dashboard, name='dashboard'),
     path('forgot-password/', view=views.forgot_password_view, name='pages_forgot_password'),
     path('verification-code/', view=views.verification_code_view, name='pages_verification_code'),
 ]
